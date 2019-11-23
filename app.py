f3f7f6b2d01d3dcb55ab28b472d0b24a3752f4fc
Felix.B <felix.b@thewalkingnerds.com>
2019-11-23 15:19:17 +0700

    all subcategories crawled

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sql
+import database
+import crawl
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    categories = sql.get_categories(0)
-    category = sql.get_category()
-    numberOfCategories = sql.get_number_of_categories()
-    return render_template('index.html', categories=categories, numberOfCategories=numberOfCategories, category=category)
+    database.create_category_table()
+    category = database.get_category()
+    mainCategories = database.get_main_categories(0)
+    numberOfMainCategories = database.get_number_of_main_categories()
+    subcategories = database.get_subcategories()
+    return render_template('index.html', subcategories=subcategories, mainCategories=mainCategories, numberOfMainCategories=numberOfMainCategories, category=category)
+    # return render_template('index.html', categories=categories, numberOfCategories=numberOfCategories)
+    # return render_template('index.html')
 
 
-@app.route('/show-more-categories')
-def show_more_categories():
-    sql.numberOfCategoriesShown += 2
+@app.route('/show-more-main-categories')
+def show_more_main_categories():
+    database.numberOfMainCategoriesShown += 2
     return redirect(url_for('.index'))
 
 
+@app.route('/crawl-all-subcategories')
+def crawl_all_subcategories():
+    database.crawl_all_subcategories(database.crawl_main_categories())
+    return redirect(url_for('.index'))
+
+@app.route('/crawl-all-categories')
+def crawl_all_categories():
+    database.crawl_all_categories(database.crawl_main_categories())
+    return redirect(url_for('.index'))
+
 @app.route("/get-category/<categoryId>", methods=['GET', 'POST'])
 def get_category(categoryId):
-    sql.iDselectedCategory = categoryId
+    database.iDselectedCategory = categoryId
     return redirect(url_for('.index'))
 
 
 # @app.route('/test')
 # def test():
-#     categories = sql.get_categories(0)
-#     numberOfCategories = sql.get_number_of_categories()
+#     categories = database.get_categories(0)
+#     numberOfCategories = database.get_number_of_categories()
 #     return render_template('index.html', categories=categories, numberOfCategories=numberOfCategories)
 
 
---
+++ database.py
@@ -0,0 +1,188 @@
+import psycopg2
+from bs4 import BeautifulSoup
+import requests
+from collections import deque
+
+TIKI_URL = 'https://tiki.vn/'
+
+conn = psycopg2.connect(user="felix", database="tiki", password="felixpostgre")
+conn.autocommit = True
+cursor = conn.cursor()
+
+
+def create_category_table():
+    query = """
+        CREATE TABLE IF NOT EXISTS categories(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(255),
+            url TEXT,
+            parent_id INT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+    """
+    try:
+        cursor.execute(query)
+        crawl_main_categories(True)
+    except Exception as err:
+        print(f'ERROR: {err}')
+
+
+def crawl_main_categories(save_db=False):
+    s = parse(TIKI_URL)
+    category_list = []
+    # Scrape through the navigator bar on Tiki homepage
+    for i in s.findAll('a', {'class': 'MenuItem__MenuLink-tii3xq-1 efuIbv'}):
+        # new category has no id
+        cat_id = None
+        # Get the category name
+        name = i.find('span', {'class': 'text'}).text
+        # Get the url value
+        url = i['href'] + "&page=1"
+        # main categories has no parent
+        parent_id = None
+
+        # Add category and url values to list
+        cat = Category(None, name, url, parent_id)
+        if save_db:
+            cat.save_into_db()
+        category_list.append(cat)
+
+    return category_list
+
+
+class Category:
+    def __init__(self, cat_id, name, url, parent_id):
+        self.cat_id = cat_id
+        self.name = name
+        self.url = url
+        self.parent_id = parent_id
+
+    def save_into_db(self):
+
+        query = 'SELECT url FROM categories WHERE url LIKE %s;'
+        val = (self.url,)
+        try:
+            cursor.execute(query, val)
+            result = cursor.fetchall()
+            if len(result) > 0:
+                return ''
+        except Exception as err:
+            print(f'ERROR: {err}')
+
+        query = f"""
+            INSERT INTO categories (name, url, parent_id) 
+            VALUES (%s, %s, %s) RETURNING id;
+        """
+        val = (self.name, self.url, self.parent_id)
+        print(
+            f'ID: {self.cat_id}, Parent ID: {self.parent_id}')
+        try:
+            cursor.execute(query, val)
+            # Get id of the new row
+            self.cat_id = cursor.fetchone()[0]
+        except Exception as err:
+            print(f'ERROR: {err}')
+
+    def __repr__(self):
+        return f'ID: {self.cat_id}, Name: {self.name}, URL: {self.url}, Parent ID: {self.parent_id}'
+
+
+def parse(url):
+    try:
+        response = requests.get(url).text
+        response = BeautifulSoup(response, "html.parser")
+        return response
+    except Exception as err:
+        print(f'ERROR: {err}')
+        return ''
+
+
+def crawl_sub_categories(category, save_db=False):
+    name = category.name
+    url = category.url
+    sub_categories = []
+
+    try:
+        div_containers = parse(url).find_all(
+            'div', attrs={"class": "list-group-item is-child"})
+        for div in div_containers:
+            sub_id = None
+            sub_name = div.a.text
+            sub_url = 'https://tiki.vn' + div.a.get('href')
+            sub_parent_id = category.cat_id
+
+            cat = Category(sub_id, sub_name, sub_url, sub_parent_id)
+            if save_db:
+                cat.save_into_db()
+            if cat.cat_id is not None:
+                sub_categories.append(cat)
+    except Exception as err:
+        print(f'ERROR: {err}')
+
+    return sub_categories
+
+
+def crawl_all_subcategories(main_categories):
+    queue = deque(main_categories)
+    count = 0
+    while queue:
+        parent_cat = queue.popleft()
+        sub_list = crawl_sub_categories(parent_cat, save_db=True)
+        queue.extend(sub_list)
+
+        # sub_list is empty, which mean the parent_cat has no sub-categories
+        if not sub_list:
+            count += 1
+            if count % 100 == 0:
+                print(f'{count} number of deepest nodes')
+
+
+numberOfMainCategoriesShown = 10
+iDselectedCategory = 32
+
+
+def get_main_categories(numberOfMainCategoriesToAdd):
+    # def get_main_categories():
+    global numberOfMainCategoriesShown
+    cursor.execute("SELECT * FROM categories WHERE parent_id IS NULL LIMIT " +
+                   str(numberOfMainCategoriesShown + numberOfMainCategoriesToAdd))
+    mainCategories = cursor.fetchall()
+    numberOfMainCategoriesShown += numberOfMainCategoriesToAdd
+    return mainCategories
+
+
+def get_category():
+    global iDselectedCategory
+    cursor.execute("SELECT * FROM categories WHERE id=" +
+                   str(iDselectedCategory))
+    category = cursor.fetchall()
+    return category[0]
+
+
+def get_subcategories():
+    global iDselectedCategory
+    cursor.execute("SELECT * FROM categories WHERE parent_id=" +
+                   str(iDselectedCategory))
+    # cursor.execute("SELECT * FROM categories LIMIT 15 OFFSET 200")
+    subcategories = cursor.fetchall()
+    return subcategories
+
+
+def get_number_of_main_categories():
+    cursor.execute("SELECT COUNT(*) FROM categories WHERE parent_id IS NULL")
+    numberOfMainCategories = cursor.fetchall()
+    return numberOfMainCategories
+
+
+# import the data from the JSON file
+# insert_query = "INSERT INTO student VALUES {}".format("(4, 'felix', 'hello@dataquest.io')")
+# cur.execute(insert_query)
+# conn.commit()
+
+  # categories = []
+  # import psycopg2
+  # conn = psycopg2.connect("dbname=tiki user=felix password=felixpostgre")
+  # cur = conn.cursor()
+  # cur.execute('SELECT * FROM categories')
+  # categories = cur.fetchall()
+  # conn.close()
