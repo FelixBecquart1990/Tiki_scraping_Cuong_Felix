199be0f082b0296832c2a999a8426455881edf23
Felix Becquart <felixbecquart1990@gmail.com>
2019-11-25 10:17:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,44 @@
 from flask import Flask, render_template, request, redirect, url_for
 import categories
 import products
+from flask_wtf import FlaskForm
+from wtforms import StringField, DecimalField, SubmitField, DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'a_secret_key'
+
+# Form
 
 
-@app.route('/')
+class SearchForm(FlaskForm):
+    search = StringField('Search for a product',
+                         validators=[DataRequired()])
+    submit = SubmitField('Search')
+
+    def __repr__(self):
+        return f"SearchForm('{self.search}')"
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # if search, return result
+    form = SearchForm()
+    if form.validate_on_submit():
+        productsToShow = products.get_search_products(form.search.data)
+    # else, return products from selected category
+    else:
+        productsToShow = products.get_products()
     categories.create_categories_table()
     products.create_products_table()
     category = categories.get_category()
     mainCategories = categories.get_main_categories(0)
     subcategories = categories.get_subcategories()
-    productsToShow = products.get_products()
     numberOfMainCategories = categories.get_number_of_main_categories()
     numberOfSubcategories = categories.get_number_of_subcategories()
     numberOfProducts = products.get_number_of_products()
     numberOfProductsOfCategory = products.get_number_of_products_of_category()
-    return render_template('index.html', numberOfProductsOfCategory=numberOfProductsOfCategory, numberOfProducts=numberOfProducts, products=productsToShow, subcategories=subcategories, mainCategories=mainCategories, numberOfMainCategories=numberOfMainCategories, numberOfSubcategories=numberOfSubcategories, category=category)
-    # return render_template('index.html', categories=categories, numberOfCategories=numberOfCategories)
-    # return render_template('index.html')
+    return render_template('index.html', form=form, numberOfProductsOfCategory=numberOfProductsOfCategory, numberOfProducts=numberOfProducts, products=productsToShow, subcategories=subcategories, mainCategories=mainCategories, numberOfMainCategories=numberOfMainCategories, numberOfSubcategories=numberOfSubcategories, category=category)
 
 
 @app.route('/show-more-main-categories')
@@ -58,21 +77,11 @@ def show_more_products():
     products.numberOfProductsToShow += 3
     return redirect(url_for('.index'))
 
-# @app.route('/test-crawl-products')
-# def test_crawl_products():
-#     products.crawl_products(True)
-#     return redirect(url_for('.index'))
-
 
 @app.route('/crawl-all-products-of-all-categories')
 def crawl_all_products_of_all_categories():
     products.crawl_all_products_of_all_categories()
     return redirect(url_for('.index'))
-# @app.route('/test')
-# def test():
-#     categories = categories.get_categories(0)
-#     numberOfCategories = categories.get_number_of_categories()
-#     return render_template('index.html', categories=categories, numberOfCategories=numberOfCategories)
 
 
 if __name__ == '__main__':
