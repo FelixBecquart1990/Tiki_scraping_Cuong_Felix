005ef42f6daa4c99644eb69956e4b9b744472674
Felix Becquart <felixbecquart1990@gmail.com>
2019-11-25 10:24:53 +0700

    Update products.py

--- products.py
+++ products.py
@@ -25,31 +25,8 @@ def create_products_table():
     """
     try:
         cursor.execute(query)
-        # crawl_main_categories(True)
     except Exception as err:
         print(f'ERROR: {err}')
-
-# def crawl_main_categories(save_db=False):
-#     s = parse(TIKI_URL)
-#     category_list = []
-#     # Scrape through the navigator bar on Tiki homepage
-#     for i in s.findAll('a', {'class': 'MenuItem__MenuLink-tii3xq-1 efuIbv'}):
-#         # new category has no id
-#         cat_id = None
-#         # Get the category name
-#         name = i.find('span', {'class': 'text'}).text
-#         # Get the url value
-#         url = i['href'] + "&page=1"
-#         # main categories has no parent
-#         parent_id = None
-
-#         # Add category and url values to list
-#         cat = Category(None, name, url, parent_id)
-#         if save_db:
-#             cat.save_into_db()
-#         category_list.append(cat)
-
-#     return category_list
 
 
 class Product:
@@ -61,29 +38,14 @@ class Product:
         self.final_price = final_price
 
     def save_into_db(self):
-
-        # query = 'SELECT url FROM products WHERE url LIKE %s;'
-        # val = (self.url,)
-        # try:
-        #     cursor.execute(query, val)
-        #     result = cursor.fetchall()
-        #     if len(result) > 0:
-        #         return ''
-        # except Exception as err:
-        #     print(f'ERROR: {err}')
-
         query = f"""
             INSERT INTO products (title, image, category_id, product_url, final_price)
             VALUES (%s, %s, %s, %s, %s) RETURNING id;
         """
         val = (self.title, self.image, self.category_id,
                self.product_url, self.final_price)
-        # print(
-        #     f'title: {self.title}')
         try:
             cursor.execute(query, val)
-            # Get id of the new row
-            # self.product_id = cursor.fetchone()[0]
         except Exception as err:
             print(f'ERROR: {err}')
 
@@ -102,8 +64,6 @@ def parse(url):
 
 
 def crawl_products(url, category_id, save_db=False):
-    # title = product.title
-    # image = product.image
 
     try:
         div_containers = parse(url).find_all(
@@ -126,7 +86,7 @@ def crawl_products(url, category_id, save_db=False):
     except Exception as err:
         print(f'ERROR: {err}')
 
-    return print("end of page crawling. Category_id:" + str(category_id))
+    return print("end of page crawling.")
 
 
 def crawl_all_products_of_all_categories():
@@ -141,53 +101,24 @@ def crawl_all_products_of_all_categories():
         else:
             url = a+"&page="
         i = 1
-        while crawl_products(url+str(i), category_id, True) != "no more products" and i < 2:
+        while crawl_products(url+str(i), category_id, True) != "no more products":
             print("page " + str(i))
             i += 1
 
 
-# def crawl_all_subcategories(main_categories):
-#     queue = deque(main_categories)
-#     count = 0
-#     while queue:
-#         parent_cat = queue.popleft()
-#         sub_list = crawl_sub_categories(parent_cat, save_db=True)
-#         queue.extend(sub_list)
-
-#         # sub_list is empty, which mean the parent_cat has no sub-categories
-#         if not sub_list:
-#             count += 1
-#             if count % 100 == 0:
-#                 print(f'{count} number of deepest nodes')
-
-
-# numberOfMainCategoriesShown = 10
-# iDselectedCategory = 32
-
-
-# def get_main_categories(numberOfMainCategoriesToAdd):
-#     # def get_main_categories():
-#     global numberOfMainCategoriesShown
-#     cursor.execute("SELECT * FROM categories WHERE parent_id IS NULL LIMIT " +
-#                    str(numberOfMainCategoriesShown + numberOfMainCategoriesToAdd))
-#     mainCategories = cursor.fetchall()
-#     numberOfMainCategoriesShown += numberOfMainCategoriesToAdd
-#     return mainCategories
-
-
-# def get_category():
-#     global iDselectedCategory
-#     cursor.execute("SELECT * FROM categories WHERE id=" +
-#                    str(iDselectedCategory))
-#     category = cursor.fetchall()
-#     return category[0]
 numberOfProductsToShow = 3
 
 
 def get_products():
     cursor.execute("SELECT * FROM products WHERE category_id =" +
                    str(categories.iDselectedCategory) + " LIMIT " + str(numberOfProductsToShow))
-    # cursor.execute("SELECT * FROM categories LIMIT 15 OFFSET 200")
+    products = cursor.fetchall()
+    return products
+
+
+def get_search_products(search):
+    cursor.execute("SELECT * FROM products WHERE title LIKE  '%" +
+                   str(search) + "%' LIMIT 9")
     products = cursor.fetchall()
     return products
 
@@ -203,16 +134,3 @@ def get_number_of_products_of_category():
                    str(categories.iDselectedCategory))
     products = cursor.fetchall()
     return products
-
-# import the data from the JSON file
-# insert_query = "INSERT INTO student VALUES {}".format("(4, 'felix', 'hello@dataquest.io')")
-# cur.execute(insert_query)
-# conn.commit()
-
-  # categories = []
-  # import psycopg2
-  # conn = psycopg2.connect("dbname=tiki user=felix password=felixpostgre")
-  # cur = conn.cursor()
-  # cur.execute('SELECT * FROM categories')
-  # categories = cur.fetchall()
-  # conn.close()
